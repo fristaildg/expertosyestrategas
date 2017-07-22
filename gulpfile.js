7e9181c7dba596144079f47300a9849b12ca0af4
fristaildg <fristaildg@gmail.com>
2017-07-21 23:37:25 -0300

    removes node_modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 var browserSync = require('browser-sync');
 
 gulp.task("sync", function() {
@@ -14,6 +16,20 @@ gulp.task("css", function() {
 		.pipe(browserSync.reload({stream:true}))
 });
 
+gulp.task("js", function() {
+	gulp.src([
+		"node_modules/slick-carousel/node_modules/jquery/dist/jquery.js",
+		"node_modules/slick-carousel/slick/slick.js",
+		"node_modules/remodal/dist/remodal.min.js",
+		"src/js/app.js"	
+	])
+
+	.pipe(concat("app.min.js"))
+	.pipe(uglify())
+	.pipe(gulp.dest("dest/js"))
+	.pipe(browserSync.reload({stream:true}))
+});
+
 gulp.task("html", function() {
 	return gulp.src("./*.html")
 		.pipe(browserSync.reload({stream:true}))
@@ -25,4 +41,6 @@ gulp.task("watch", function() {
 });
 
 gulp.task("arrancar", ["sync", "watch"]);
+
+gulp.task("default", ["js"]);
 
